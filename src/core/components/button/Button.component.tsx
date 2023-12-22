698321a1967b29e206daf9881a2a744a1ee10a25
Igor José Castro Mariano Lima <igormariano@outlook.com.br>
2023-12-22 20:57:10 -0300

    portfolio-react: Feat - Add 'ghost' button template

--- src/core/components/button/Button.component.tsx
+++ src/core/components/button/Button.component.tsx
@@ -8,15 +8,28 @@ export const Button = ({
     variant = 'primary',
     ...props
 }: PropsWithChildren<ButtonProps>) => {
-    if (variant === 'primary') return <ButtonPrimary {...props}>{children}</ButtonPrimary>;
+    if (variant === 'primary') return <ButtonPrimary {...props}>{ children }</ButtonPrimary>;
+    if (variant === 'ghost') return <ButtonGhost {...props}>{ children }</ButtonGhost>
 }
 
 export const ButtonPrimary = ({
-    children
-}: PropsWithChildren) => {
+    children,
+    ...props
+}: PropsWithChildren<ButtonProps>) => {
     return(
-        <S.BtnPrimary>
-            <span className='h6'>{children}</span>
+        <S.BtnPrimary {...props}>
+            <span className='h6'>{ children }</span>
         </S.BtnPrimary>
+    )
+}
+
+export const ButtonGhost = ({
+    children,
+    ...props
+}: PropsWithChildren<ButtonProps>) => {
+    return(
+        <S.BtnGhost {...props}>
+            <span className='h6'>{ children }</span>
+        </S.BtnGhost>
     )
 }
