edf62600b398ef4014f51e02db01ffc2c547f263
Igor José Castro Mariano Lima <igormariano@outlook.com.br>
2023-12-23 16:38:04 -0300

    portfolio-react: Feat - Add 'Sql' icon component

--- src/shared/components/icon/icons/index.ts
+++ src/shared/components/icon/icons/index.ts
@@ -5,6 +5,7 @@ import { LinkedIn } from './LinkedIn.icon';
 import { Nest } from './Nest.icon';
 import { React } from './React.icon';
 import { ReactNative } from './ReactNative.icon';
+import { Sql } from './Sql.icon';
 
 export const icons = {
     'angular': Angular,
@@ -13,7 +14,8 @@ export const icons = {
     'gitHub': GitHub,
     'react': React,
     'nest': Nest,
-    'reactNative': ReactNative
+    'reactNative': ReactNative,
+    'sql': Sql
 }
 
 export type IconName = keyof typeof icons;
