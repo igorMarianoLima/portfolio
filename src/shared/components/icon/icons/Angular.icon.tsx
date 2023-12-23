3d8145ce83837381f74aa30163ab6146feae64b6
Igor José Castro Mariano Lima <igormariano@outlook.com.br>
2023-12-23 16:08:52 -0300

    portfolio-react: Feat - Create 'LinkedIn', 'GitLab' and 'GitHub' icons components

--- src/shared/components/icon/icons/Angular.icon.tsx
+++ src/shared/components/icon/icons/Angular.icon.tsx
@@ -1,9 +1,15 @@
 import { SVGProps } from "react"
 
-export const Angular = (props: SVGProps<SVGSVGElement>) => {
+export const Angular = (
+{
+    width,
+    height,
+    fill,
+    ...props
+}: SVGProps<SVGSVGElement>) => {
     return(
-        <svg width={props.width || 32} height={props.height || 32} viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M15.8884 0L30.7768 5.30526L28.52 25.0105L15.8884 32L3.25684 25.0105L1 5.30526L15.8884 0ZM15.8884 3.53684L6.57474 24.4211H10.0442L11.9137 19.7389H19.8295L21.6989 24.4211H25.1516L15.8884 3.53684ZM18.6168 16.8421H13.1768L15.8884 10.3242L18.6168 16.8421Z" fill={props.fill || "#FBFBFB"}/>
+        <svg {...props} width={width || 32} height={height || 32} viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M15.8884 0L30.7768 5.30526L28.52 25.0105L15.8884 32L3.25684 25.0105L1 5.30526L15.8884 0ZM15.8884 3.53684L6.57474 24.4211H10.0442L11.9137 19.7389H19.8295L21.6989 24.4211H25.1516L15.8884 3.53684ZM18.6168 16.8421H13.1768L15.8884 10.3242L18.6168 16.8421Z" fill={fill || "var(--white-100)"}/>
         </svg>
     )
 }
--- src/shared/components/icon/icons/index.ts
+++ src/shared/components/icon/icons/index.ts
@@ -1,7 +1,13 @@
 import { Angular } from './Angular.icon';
+import { GitHub } from './GitHub.icon';
+import { GitLab } from './GitLab.icon';
+import { LinkedIn } from './LinkedIn.icon';
 
 export const icons = {
-    'angular': Angular
+    'angular': Angular,
+    'linkedIn': LinkedIn,
+    'gitLab': GitLab,
+    'gitHub': GitHub
 }
 
 export type IconName = keyof typeof icons;
