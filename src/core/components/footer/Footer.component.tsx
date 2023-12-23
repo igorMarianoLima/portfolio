179403f42b3990e8e0f45c15299b6c98e70e3c0f
Igor José Castro Mariano Lima <igormariano@outlook.com.br>
2023-12-23 20:56:48 -0300

    portfolio-react: Refactor - Use social links from constants in 'Footer' component

--- src/core/components/footer/Footer.component.tsx
+++ src/core/components/footer/Footer.component.tsx
@@ -1,34 +1,16 @@
-import { SocialLink } from '../../../shared/components/socialLink/SocialLink.component';
-import { SocialLinkProps } from '../../../shared/components/socialLink/SocialLink.interface';
+import { SOCIAL_LINKS } from '../../../constants/socialLinks';
 
+import { SocialLink } from '../../../shared/components/socialLink/SocialLink.component';
 import * as S from './Footer.styles';
 
 export const Footer = () => {
-    const socialLinks: SocialLinkProps[] = [
-        {
-            icon: 'linkedIn',
-            label: 'LinkedIn',
-            redirectTo: 'https://www.linkedin.com/in/igor-mariano-lima/'
-        },
-        {
-            icon: 'gitLab',
-            label: 'GitLab',
-            redirectTo: 'https://gitlab.com/igorMarianoLima/'
-        },
-        {
-            icon: 'gitHub',
-            label: 'GitHub',
-            redirectTo: 'https://github.com/igorMarianoLima/'
-        }
-    ];
-
     return(
         <S.FooterWrapper>
             <S.FooterContainer>
                 <h1 className='h4'>Igor Mariano</h1>
 
                 <S.SocialLinks>
-                    {socialLinks.map(link => <SocialLink key={link.label} {...link} />)}
+                    {SOCIAL_LINKS.map(link => <SocialLink key={link.label} {...link} />)}
                 </S.SocialLinks>
             </S.FooterContainer>
         </S.FooterWrapper>
