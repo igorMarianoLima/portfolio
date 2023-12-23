d4d77870c89ec544b3d479e82f17002e192db9bf
Igor José Castro Mariano Lima <igormariano@outlook.com.br>
2023-12-23 16:26:47 -0300

    portfolio-react: Feat - Add 'React' icon component

--- src/shared/components/icon/icons/index.ts
+++ src/shared/components/icon/icons/index.ts
@@ -2,12 +2,14 @@ import { Angular } from './Angular.icon';
 import { GitHub } from './GitHub.icon';
 import { GitLab } from './GitLab.icon';
 import { LinkedIn } from './LinkedIn.icon';
+import { React } from './React.icon';
 
 export const icons = {
     'angular': Angular,
     'linkedIn': LinkedIn,
     'gitLab': GitLab,
-    'gitHub': GitHub
+    'gitHub': GitHub,
+    'react': React
 }
 
 export type IconName = keyof typeof icons;
